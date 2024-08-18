e39a8e318f2a264d09b6940db917f2ffe1fbb5ca
elijah-fr <elijahkurien05@gmail.com>
2024-08-18 12:26:56 -0500

    Pushed  project_library parser

--- bioelectric_cell_reprogramming/ion_concentration_biomodulator/scripts/add_project_libraries.sh
+++ bioelectric_cell_reprogramming/ion_concentration_biomodulator/scripts/add_project_libraries.sh
@@ -1,1 +1,16 @@
 # Parse downloaded EDA models to store in the repository
+
+accepted_extensions=("kicad_sym" "kicad_mod" "step" "stp")
+remote_model_path="../user_downloads/"
+file_path="../user_downloads/2x04_socket_IPL1-104-01-L-D-K.pretty" # Change to the path where your EDA model was downloaded 
+new_name="test_123.pretty" # Format descriptions as Name_Part#.pretty (e.g. Barrel_Jack_EJ508A.pretty)
+
+
+new_dir="$remote_model_path/$new_name"
+mkdir -p "$new_dir"
+
+for extension in "${accepted_extensions[@]}"
+do
+    find "$file_path" -type f -name "*.$extension" -exec cp {} "$new_dir" \;
+done
+
